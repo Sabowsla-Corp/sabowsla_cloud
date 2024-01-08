ebf74e027646d81d84fcb699eaf8bf5e43fa7e46
Sabowsla <sabowsla@gmail.com>
2024-01-08 04:10:41 -0500

    add macOs support

---
+++ linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <image_compression_flutter/image_compression_flutter_plugin.h>
+#include <objectbox_flutter_libs/objectbox_flutter_libs_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) image_compression_flutter_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "ImageCompressionFlutterPlugin");
+  image_compression_flutter_plugin_register_with_registrar(image_compression_flutter_registrar);
+  g_autoptr(FlPluginRegistrar) objectbox_flutter_libs_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "ObjectboxFlutterLibsPlugin");
+  objectbox_flutter_libs_plugin_register_with_registrar(objectbox_flutter_libs_registrar);
+}
